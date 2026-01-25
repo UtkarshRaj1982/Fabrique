b26de69ac720ed63c30fb32b6794fc37a1fdc9cd
UtkarshRaj1982 <utkarshraj4321@gmail.com>
2026-01-25 13:00:44 +0530

    Corrected font

--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
     <>
       <nav className="container mx-auto flex items-center justify-between py-4 px-6">
         {/* Logo */}
-        <Link to="/" className="text-2xl font-semibold tracking-tight">
+        <Link to="/" className="text-2xl tracking-tight fabrique-logo">
           Fabrique
         </Link>
 
